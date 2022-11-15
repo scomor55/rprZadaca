07b6938e14748ccf6ef692e2665942d4ab696418
scomor55 <scomor2@etf.unsa.ba>
2022-11-15 20:10:25 +0100

    update

--- rprZadaca/src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ rprZadaca/src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -14,17 +14,29 @@ public class ExpressionEvaluatorTest {
     @Test
     void evaluate1() {
         /**
-         * Testiranje mnozenja
+         * Testiranje sabiranja
          *
          */
 
         String rez;
-        rez = String.valueOf(ExpressionEvaluator.evaluate("( 4 * 5 ) "));
-        assertEquals("20.0",rez);
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( ( 1 + 2 ) + ( 3 + 4 ) )"));
+        assertEquals("10.0",rez);
     }
+
 
     @Test
     void evaluate2() {
+        /**
+         * Testiranje mnozenja
+         *
+         */
+        String rez;
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( ( 2 * 3 ) * ( 4 * 5 ) )"));
+        assertEquals("120.0",rez);
+    }
+
+    @Test
+    void evaluate3() {
         /**
          * Testiranje slozenijeg izraza
          *
@@ -35,7 +47,7 @@ public class ExpressionEvaluatorTest {
     }
 
     @Test
-    void evaluate3() {
+    void evaluate4() {
         /**
          * Testiranje slozenijeg izraza
          *
@@ -46,7 +58,7 @@ public class ExpressionEvaluatorTest {
     }
 
     @Test
-    void evaluate4() {
+    void evaluate5() {
         /**
          * Testiranje slozenijeg izraza
          *
@@ -57,7 +69,7 @@ public class ExpressionEvaluatorTest {
     }
 
     @Test
-    void evaluate5() {
+    void evaluate6() {
         /**
          * Testiranje slozenijeg izraza
          *
@@ -65,5 +77,81 @@ public class ExpressionEvaluatorTest {
         String rez;
         rez = String.valueOf(ExpressionEvaluator.evaluate("( 1 + ( 5 * 20 ) )"));
         assertEquals("101.0",rez);
+    }
+
+    @Test
+    void evaluate7() {
+        /**
+         * Testiranje sqrt operatora
+         *
+         */
+        String rez;
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( sqrt ( 4 ) + ( 5 * 20 ) )"));
+        assertEquals("102.0",rez);
+    }
+
+
+    @Test
+    void evaluate8() {
+        /**
+         * Testiranje primjera iz zadace
+         *
+         */
+        String rez;
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"));
+        assertEquals("101.0",rez);
+    }
+
+    @Test
+    void evaluate9() {
+        /**
+         * Testiranje dijeljenja
+         *
+         */
+        String rez;
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( ( 720 / 6 ) / ( 24 / 2 ) )"));
+        assertEquals("10.0",rez);
+    }
+
+    @Test
+    void evaluate10() {
+        /**
+         * Testiranje oduzimanja
+         *
+         */
+        String rez;
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( ( 27 - 7 ) - ( 12 - 4 ) )"));
+        assertEquals("12.0",rez);
+    }
+
+    @Test
+    void evaluate11() {
+        /**
+         * Testiranje slozenijeg izraza
+         *
+         */
+        String rez;
+        rez = String.valueOf(ExpressionEvaluator.evaluate("( ( sqrt ( 625 ) - sqrt ( 225 ) ) - ( sqrt ( 100 ) - sqrt ( 25 ) ) )"));
+        assertEquals("5.0",rez);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        /**
+         * Testiranje izuzetka
+         *
+         */
+        assertThrows(RuntimeException.class, () -> { ExpressionEvaluator.evaluate("( 2 +2 )");});
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        /**
+         * Testiranje izuzetka
+         *
+         */
+        assertThrows(RuntimeException.class, () -> { ExpressionEvaluator.evaluate("( 1 + ( ( 2 + 3 )* ( 4 * 5 ) ) )");});
     }
 }
